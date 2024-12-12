5a34b342d94cafaeb310c3d0d2edd7b066f0c6e6
Piero del Valle <devapeu@gmail.com>
2024-12-12 11:22:47 -0500

    add recipes store

---
+++ frontend/src/stores/useRecipes.js
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useRecipesStore = defineStore('recipes', () => {
+  const recipes = ref([])
+
+  function get() {
+    fetch('http://localhost:8000/recipes')
+      .then(response => response.json())
+      .then(data => recipes.value = data.recipes)
+  }
+
+  function getSingle(id) {
+    fetch(`http://localhost:8000/recipes/${id}`)
+      .then(response => response.json())
+      .then(data => recipes.value = data.recipes)
+  }
+
+  function add(recipe) {
+    fetch('http://localhost:8000/recipes', {
+      method: 'POST',
+      body: JSON.stringify(recipe)
+    })
+    .then(response => response.json())
+    .then(data => recipes.value = data.recipes)
+  }
+
+  function remove(id) {
+    fetch(`http://localhost:8000/recipes/${id}`, {
+      method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(data => recipes.value = data.recipes)
+  }
+
+  return { recipes, get, getSingle, add, remove }
+})
