990d467e9c1d08c32798bc8742dec4cf8f6cc7be
Piero del Valle <devapeu@gmail.com>
2025-11-07 22:46:05 -0500

    remove index

--- api/migrations/add_position_to_shopping_list.sql
+++ api/migrations/add_position_to_shopping_list.sql
@@ -3,6 +3,3 @@ ALTER TABLE shopping_list ADD COLUMN position INT;
 
 -- Initialize positions based on current IDs
 UPDATE shopping_list SET position = id;
-
--- Create unique index on position to ensure order integrity
-ALTER TABLE shopping_list ADD UNIQUE INDEX idx_position (position);
