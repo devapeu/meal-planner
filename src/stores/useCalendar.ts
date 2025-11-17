2717b986d6e2ab831f783c1067a6c9e84b0cc7d6
Piero del Valle <devapeu@gmail.com>
2025-11-17 14:51:00 -0500

    refactor: add types for calendar

--- src/stores/useCalendar.ts
+++ src/stores/useCalendar.ts
@@ -1,32 +1,39 @@
+import { CalendarItem, CalendarResponse } from '@/types/calendar';
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 const VITE_API_URL = import.meta.env.VITE_API_URL
 
 export const useCalendarStore = defineStore('calendar', () => {
-  const calendar = ref([]);
+  const calendar = ref<CalendarItem[]>([]);
 
-  function getMealsForWeek(startDate) {
+  function getMealsForWeek(startDate: string) {
     fetch(`${VITE_API_URL}/calendar?startDate=${startDate}`)
-      .then(response => response.json())
-      .then(data => calendar.value = data.calendar)
+      .then(async response => {
+        const data: CalendarResponse = await response.json();
+        calendar.value = data.calendar;
+      })
   }
 
-  function add(meal, startDate, endDate) {
+  function add(meal: string, startDate: string, endDate: string) {
     fetch(`${VITE_API_URL}/calendar`, {
       method: 'POST',
       body: JSON.stringify({ meal, startDate, endDate })
     })
-    .then(response => response.json())
-    .then(data => calendar.value = data.calendar)
+    .then(async response => {
+      const data: CalendarResponse = await response.json();
+      calendar.value = data.calendar;
+    })
   }
 
-  function remove(id, startDate) {
+  function remove(id: string, startDate: string) {
     fetch(`${VITE_API_URL}/calendar/${id}?startDate=${startDate}`, {
       method: 'DELETE'
     })
-    .then(response => response.json())
-    .then(data => calendar.value = data.calendar)
+    .then(async response => {
+      const data: CalendarResponse = await response.json();
+      calendar.value = data.calendar;
+    })
   }
 
   return { calendar, getMealsForWeek, add, remove }
