a09415f39a9c0b2ccae3d727802a9633e5cbfa62
Piero del Valle <devapeu@gmail.com>
2025-11-17 10:48:11 -0500

    remove unused routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
-import RecipesListView from '@/views/RecipesListView.vue';
-import RecipesSingleView from '@/views/RecipesSingleView.vue';
-import RecipesNewView from '@/views/RecipesNewView.vue';
 
 const routes = [
   { path: '/', component: HomeView, name: 'home', meta: { title: 'Home' } },
-  { path: '/recipes', component: RecipesListView, name: 'recipes', meta: { title: 'Recipes' } },
-  { path: '/recipes/:id', component: RecipesSingleView, name: 'recipes-single' },
-  { path: '/recipes/new', component: RecipesNewView, name: 'recipes-new', meta: { title: 'New Recipe' } },
 ];
 
 const router = createRouter({
