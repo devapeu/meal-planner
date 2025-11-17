0c7f2869ea01f82cec5d86c507582174952740dc
Piero del Valle <devapeu@gmail.com>
2025-11-17 12:38:55 -0500

    refactor: use typescript

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,9 @@
+// Source - https://stackoverflow.com/questions/63724523/how-to-add-typescript-to-vue-3-and-vite-project
+// Posted by gg.jiang
+// Retrieved 2025-11-17, License - CC BY-SA 4.0
+
+declare module "*.vue" {
+  import { defineComponent } from "vue";
+  const Component: ReturnType<typeof defineComponent>;
+  export default Component;
+}
