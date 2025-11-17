f721d0e301997cb54393df5a8579159675004ecc
Piero del Valle <devapeu@gmail.com>
2025-11-17 15:27:39 -0500

    fix: refactor to ts

--- src/components/calendar/getColorFromId.ts
+++ src/components/calendar/getColorFromId.ts
@@ -1,5 +1,5 @@
-export function getColorFromId(id) {
-  const index = (id - 1 ) % 8;
+export function getColorFromId(id: number): string {
+  const index = (id - 1) % 8;
   const colors = ['#dbeafe', '#fee2e2', '#d1fae5', '#fef3c7', '#ede9fe', '#ffedd5', '#fce7f3', '#ccfbf1']
   return colors[index];
 }
