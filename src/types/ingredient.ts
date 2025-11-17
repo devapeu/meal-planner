d54645eb73ff81c3376f765080c6445cd2d343b2
Piero del Valle <devapeu@gmail.com>
2025-11-17 15:26:12 -0500

    fix: update type specs

--- src/types/ingredient.ts
+++ src/types/ingredient.ts
@@ -2,5 +2,5 @@ export interface Ingredient {
   name: string,
   quantity: number | null,
   measure: string | null,
-  notes: string | null,
+  note: string | null,
 }
--- src/types/section.ts
+++ src/types/section.ts
@@ -1,6 +1,7 @@
 import { Ingredient } from "./ingredient";
 
 export interface Section {
+  name: string,
   ingredients: Ingredient[],
   instructions: string[],
   utensils: string[],
