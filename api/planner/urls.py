406e292a2052a8037d762d3136efb0d146ac97c6
Piero del Valle <devapeu@gmail.com>
2025-11-24 17:01:04 -0500

    fix trailing slashes

--- api/planner/urls.py
+++ api/planner/urls.py
@@ -12,24 +12,24 @@ from .views import (
 )
 
 urlpatterns = [
-    path("auth/register", RegisterView.as_view(), name="auth-register"),
-    path("auth/login", LoginView.as_view(), name="auth-login"),
-    path("auth/logout", LogoutView.as_view(), name="auth-logout"),
-    path("auth/user", CurrentUserView.as_view(), name="auth-user"),
-    path("calendar", CalendarView.as_view(), name="calendar"),
-    path("calendar/<int:pk>", CalendarDetailView.as_view(), name="calendar-detail"),
-    path("shopping-list", ShoppingListView.as_view(), name="shopping-list"),
+    path("auth/register/", RegisterView.as_view(), name="auth-register"),
+    path("auth/login/", LoginView.as_view(), name="auth-login"),
+    path("auth/logout/", LogoutView.as_view(), name="auth-logout"),
+    path("auth/user/", CurrentUserView.as_view(), name="auth-user"),
+    path("calendar/", CalendarView.as_view(), name="calendar"),
+    path("calendar/<int:pk>/", CalendarDetailView.as_view(), name="calendar-detail"),
+    path("shopping-list/", ShoppingListView.as_view(), name="shopping-list"),
     path(
-        "shopping-list/<int:pk>",
+        "shopping-list/<int:pk>/",
         ShoppingListDetailView.as_view(),
         name="shopping-list-detail",
     ),
     path(
-        "shopping-list/reorder",
+        "shopping-list/reorder/",
         ShoppingListReorderView.as_view(),
         name="shopping-list-reorder",
     ),
-    path("recipes", RecipesView.as_view(), name="recipes"),
-    path("recipes/<int:pk>", RecipeDetailView.as_view(), name="recipe-detail"),
+    path("recipes/", RecipesView.as_view(), name="recipes"),
+    path("recipes/<int:pk>/", RecipeDetailView.as_view(), name="recipe-detail"),
 ]
 
